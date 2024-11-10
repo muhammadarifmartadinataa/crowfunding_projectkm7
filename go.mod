4a8a7c93b85a7a1945e4ddd32a3974b135509e66
muhammadarifmartadinataa <muhammadarifmartadinata@gmail.com>
2024-11-10 23:26:36 +0700

    JWT AUTH - GENERATE TOKEN

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.23.2
 
 require (
 	github.com/gin-gonic/gin v1.10.0
+	github.com/go-playground/validator/v10 v10.22.1
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	golang.org/x/crypto v0.28.0
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.12
@@ -19,7 +21,6 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.22.1 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
--- handler/user.go
+++ handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"crowfundig/auth"
 	"crowfundig/helper"
 	"crowfundig/user"
 	"fmt"
@@ -11,10 +12,11 @@ import (
 
 type userHandler struct {
 	userService user.Service
+	authService auth.Service
 }
 
-func NewUserHandler(userService user.Service) *userHandler {
-	return &userHandler{userService}
+func NewUserHandler(userService user.Service, authService auth.Service) *userHandler {
+	return &userHandler{userService, authService}
 }
 
 func (h *userHandler) RegisterUser(c *gin.Context) {
@@ -43,9 +45,15 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		return
 	}
 
-	//token, err :=  h.jswtService.GenerateToken()
+	token, err := h.authService.GenerateToken(int(newUser.ID))
+	if err != nil {
+		response := helper.APIResponse("Register Account failed", http.StatusBadRequest, "error", nil)
 
-	formatter := user.FormatUser(newUser, "tokentokentoken")
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, token)
 	response := helper.APIResponse("Account hass been registered", http.StatusOK, "succes", formatter)
 
 	c.JSON(http.StatusOK, response)
@@ -81,8 +89,16 @@ func (h *userHandler) Login(c *gin.Context) {
 		c.JSON(http.StatusUnprocessableEntity, response)
 		return
 	}
+
+	token, err := h.authService.GenerateToken(int(loggedinUser.ID))
+	if err != nil {
+		response := helper.APIResponse("Login failed", http.StatusBadRequest, "error", nil)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
 	//jika login berhasil maka perlu balikan loggedinUser ke dalam format json
-	formatter := user.FormatUser(loggedinUser, "tokentokentoken")
+	formatter := user.FormatUser(loggedinUser, token)
 
 	response := helper.APIResponse("Succesfuly logedin", http.StatusOK, "succes", formatter)
 
