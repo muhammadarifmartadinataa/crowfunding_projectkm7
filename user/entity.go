957ada4c7083c793efdc26627f372edc3e2237f9
muhammadarifmartadinataa <muhammadarifmartadinata@gmail.com>
2024-11-11 00:03:11 +0700

    EDIT : entity User

--- user/entity.go
+++ user/entity.go
@@ -3,16 +3,16 @@ package user
 import "time"
 
 type User struct {
-	ID             uint      `gorm:"primaryKey;autoIncrement"`
-	Name           string    `gorm:"type:varchar(255)"`
-	Occupation     string    `gorm:"type:varchar(255)"`
-	Email          string    `gorm:"type:varchar(255);unique"`
-	PasswordHash   string    `gorm:"type:varchar(255)"`
-	AvatarFileName string    `gorm:"type:varchar(255)"`
-	Role           string    `gorm:"type:varchar(50)"`
-	Token          string    `gorm:"type:varchar(255)"`
-	CreatedAt      time.Time `gorm:"type:datetime"`
-	UpdatedAt      time.Time `gorm:"type:datetime"`
+	ID             uint
+	Name           string
+	Occupation     string
+	Email          string
+	PasswordHash   string
+	AvatarFileName string
+	Role           string
+	Token          string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
 	// Campaigns      []Campaign    `gorm:"foreignKey:UserID"`
 	// Transactions   []Transaction `gorm:"foreignKey:UserID"`
 }
